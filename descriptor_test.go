e926230a7f830255a4c30c94d2b013cea0068e82
toukii <toukii@yahoo.com>
2019-06-06 15:36:03 +0800

    test

---
+++ descriptor_test.go
@@ -0,0 +1,12 @@
+package desc
+
+import (
+	"testing"
+)
+
+func TestGenDescriptorSet(t *testing.T) {
+	err := GenDescriptorSet("$GOPATH/src/github.com/tgrpc/ngrpc", ".helloworld.Greeter.pbin", "helloworld/helloworld.proto")
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+}
