158867b9f2d5fcf41cb336aff8f0a9f9625bee49
toukii <toukii@yahoo.com>
2019-07-05 11:32:21 +0800

    search pb

--- search.go
+++ search.go
@@ -7,17 +7,21 @@ import (
 
 	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"github.com/tgrpc/grpcurl"
+	"github.com/toukii/bytes"
 	"github.com/toukii/goutils"
 )
 
 type ProtoDesc struct {
 	fileDescriptorSet *descriptor.FileDescriptorSet
+	rawPbDescs        []string
 }
 
-func NewProtoDesc(fileDescriptorSet *descriptor.FileDescriptorSet, pbDirs ...string) *ProtoDesc {
+func NewProtoDesc(fileDescriptorSet *descriptor.FileDescriptorSet, rawDescs []string, pbDirs ...string) *ProtoDesc {
 	pdesc := &ProtoDesc{
 		fileDescriptorSet: fileDescriptorSet,
+		rawPbDescs:        make([]string, 0, 3+len(rawDescs)),
 	}
+	pdesc.rawPbDescs = append(pdesc.rawPbDescs, rawDescs...)
 
 	Collect(pbDirs...)
 	for key, it := range associPB {
@@ -27,20 +31,30 @@ func NewProtoDesc(fileDescriptorSet *descriptor.FileDescriptorSet, pbDirs ...str
 	return pdesc
 }
 
-func (p *ProtoDesc) searchDescSrc() (grpcurl.DescriptorSource, error) {
+func (p *ProtoDesc) searchDescSrc(method string) (grpcurl.DescriptorSource, error) {
 	var source grpcurl.DescriptorSource
 	for {
 		var err error
 		source, err = grpcurl.DescriptorSourceFromFileDescriptorSet(p.fileDescriptorSet)
-		if err == nil {
+		if err == nil && len(p.fileDescriptorSet.GetFile()) > 0 {
+			log.Errorf("DescriptorSourceFromFileDescriptorSet, err:%+v", err)
 			break
 		}
 
-		msg := err.Error()
-		protoFile := needProtoFile(msg)
-		pbgoFile := parse2PBFile(protoFile)
-		log.Debugf("%s ==> %s", msg, protoFile)
-		pbgo := Search(pbgoFile)
+		var msg, protoFile, pbgoFile string
+		var pbgo *AssociPB
+		if err == nil {
+			msg = fmt.Sprintf("no descriptor found for %s", method)
+			pkgName, _ := GetPackageName(method)
+			serviceName, _ := GetServiceName(method)
+			pbgo = SearchByPkgName(pkgName, serviceName)
+		} else {
+			msg = err.Error()
+			protoFile = needProtoFile(msg)
+			pbgoFile = parse2PBFile(protoFile)
+			log.Debugf("%s ==> %s", msg, protoFile)
+			pbgo = Search(pbgoFile)
+		}
 		if pbgo == nil {
 			panic(fmt.Sprintf("cannot find %s", pbgoFile))
 		}
@@ -57,25 +71,47 @@ func (p *ProtoDesc) searchDescSrc() (grpcurl.DescriptorSource, error) {
 		if err != nil {
 			return source, err
 		}
+		p.rawPbDescs = append(p.rawPbDescs, pbdesc)
 		p.fileDescriptorSet.File = append(p.fileDescriptorSet.File, pbdescfile)
 	}
 	return source, nil
 }
 
 func SearchDescSrcByRawDescs(method string, rawDescs []string, pbDirs ...string) (grpcurl.DescriptorSource, error) {
-	source, err := Try(method, pbDirs...)
-	return source, err
-	fileDescriptorSet, err := DecodeFileDescriptorSetByRaw("", rawDescs)
-	if err != nil {
-		return nil, err
+	var fileDescriptorSet *descriptor.FileDescriptorSet
+	var err error
+	if len(rawDescs) > 0 {
+		fileDescriptorSet, err = DecodeFileDescriptorSetByRaw("", rawDescs)
+		if err != nil {
+			log.Errorf("DecodeFileDescriptorSetByRaw, err:%+v", err)
+		}
+		fileDescriptorSet, err = DecodeFileDescriptorSet(method, fileDescriptorSet)
+		if err != nil {
+			log.Errorf("DecodeFileDescriptorSet, err:%+v", err)
+		}
 	}
-	fileDescriptorSet, err = DecodeFileDescriptorSet(method, fileDescriptorSet)
-	if err != nil {
-		return nil, err
-	}
-	pdesc := NewProtoDesc(fileDescriptorSet, pbDirs...)
 
-	return pdesc.searchDescSrc()
+	if fileDescriptorSet == nil {
+		fileDescriptorSet = &descriptor.FileDescriptorSet{
+			File: make([]*descriptor.FileDescriptorProto, 0, 3),
+		}
+	}
+
+	pdesc := NewProtoDesc(fileDescriptorSet, rawDescs, pbDirs...)
+
+	descsrc, err := pdesc.searchDescSrc(method)
+	serviceName, _ := GetServiceName(method)
+	wr := bytes.NewWriter(make([]byte, 0, 10240))
+	for i, it := range pdesc.rawPbDescs {
+		if i > 0 {
+			wr.Write([]byte(`, `))
+		}
+		wr.Write([]byte(`"`))
+		wr.Write(goutils.ToByte(it))
+		wr.Write([]byte(`"`))
+	}
+	goutils.WriteFile(fmt.Sprintf("%s.desc", serviceName), wr.Bytes())
+	return descsrc, err
 }
 
 func Try(method string, pbDirs ...string) (grpcurl.DescriptorSource, error) {
@@ -92,7 +128,7 @@ func Try(method string, pbDirs ...string) (grpcurl.DescriptorSource, error) {
 	fmt.Println("SearchByImportName:", apb)
 	for _, it := range apb.PBs {
 		pbdesc := getDescriptorBytes(goutils.ReadFile(it.AbsDir))
-		fmt.Println("pbdesc:", pbdesc)
+		// fmt.Println("pbdesc:", pbdesc)
 		fileDescriptor, err := decodeDescByRaw(pbdesc)
 		if err != nil {
 			log.Errorf("%s err:%+v", it.ImportName(), err)
@@ -101,8 +137,8 @@ func Try(method string, pbDirs ...string) (grpcurl.DescriptorSource, error) {
 		set := &descriptor.FileDescriptorSet{
 			File: []*descriptor.FileDescriptorProto{fileDescriptor},
 		}
-		pb := NewProtoDesc(set, pbDirs...)
-		source, err := pb.searchDescSrc()
+		pb := NewProtoDesc(set, []string{pbdesc}, pbDirs...)
+		source, err := pb.searchDescSrc(method)
 		if err != nil {
 			log.Errorf("searchDescSrc %s err:%+v", it.ImportName(), err)
 			continue
