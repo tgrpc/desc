2983193a1b2753e9b627789c152d3914b88b2cf1
toukii <toukii@yahoo.com>
2020-01-10 21:21:05 +0800

    search-loop

--- search.go
+++ search.go
@@ -14,30 +14,48 @@ import (
 type ProtoDesc struct {
 	fileDescriptorSet *descriptor.FileDescriptorSet
 	rawPbDescs        []string
+	unique            map[string]bool
 }
 
 func NewProtoDesc(fileDescriptorSet *descriptor.FileDescriptorSet, rawDescs []string, pbDirs ...string) *ProtoDesc {
 	pdesc := &ProtoDesc{
 		fileDescriptorSet: fileDescriptorSet,
 		rawPbDescs:        make([]string, 0, 3+len(rawDescs)),
+		unique:            make(map[string]bool, 10),
 	}
 	pdesc.rawPbDescs = append(pdesc.rawPbDescs, rawDescs...)
 
 	Collect(pbDirs...)
-	for key, it := range associPB {
-		fmt.Println(key, it)
+	log.Infof("collect %d pb.go", len(associPB))
+	for key, _ := range associPB {
+		fmt.Println(key)
 	}
 
 	return pdesc
 }
 
+func (p *ProtoDesc) addDesc(unique, pbdesc string, pbdescfile *descriptor.FileDescriptorProto) {
+	if p.unique[unique] {
+		log.Warnf("%s is duplicated", unique)
+		return
+	}
+	p.rawPbDescs = append(p.rawPbDescs, pbdesc)
+	p.fileDescriptorSet.File = append(p.fileDescriptorSet.File, pbdescfile)
+	p.unique[unique] = true
+}
+
 func (p *ProtoDesc) searchDescSrc(method string) (grpcurl.DescriptorSource, error) {
 	var source grpcurl.DescriptorSource
+
+	pkgName, _ := GetPackageName(method)
+	serviceName, _ := GetServiceName(method)
+	log.Debugf("search %s, pkg: %s, service: %s", method, pkgName, serviceName)
+
 	for {
 		var err error
 		source, err = grpcurl.DescriptorSourceFromFileDescriptorSet(p.fileDescriptorSet)
 		if err == nil && len(p.fileDescriptorSet.GetFile()) > 0 {
-			log.Errorf("DescriptorSourceFromFileDescriptorSet, err:%+v", err)
+			log.Warnf("search desc loop end")
 			break
 		}
 
@@ -45,25 +63,34 @@ func (p *ProtoDesc) searchDescSrc(method string) (grpcurl.DescriptorSource, erro
 		var pbgo *AssociPB
 		if err == nil {
 			msg = fmt.Sprintf("no descriptor found for %s", method)
-			pkgName, _ := GetPackageName(method)
-			serviceName, _ := GetServiceName(method)
 			pbgo = SearchByPkgName(pkgName, serviceName)
 		} else {
 			msg = err.Error()
 			protoFile = needProtoFile(msg)
-			pbgoFile = parse2PBFile(protoFile)
-			log.Debugf("%s ==> %s", msg, protoFile)
+			pbgoFile = pkgName + "/" + parse2PBFile(protoFile)
+			log.Debugf("%s ==> %s %s", msg, protoFile, pbgoFile)
 			pbgo = Search(pbgoFile)
 		}
 		if pbgo == nil {
 			panic(fmt.Sprintf("cannot find %s", pbgoFile))
 		}
+
 		var pbdesc string
 		find := false
 		var pbdescfile *descriptor.FileDescriptorProto
 		for _, pb := range pbgo.PBs {
-			log.Debugf("search %s ==> %s", protoFile, pb.AbsDir)
-			pbdesc = getDescriptorBytes(goutils.ReadFile(pb.AbsDir))
+			log.Debugf("searching %s", pb.ImportName())
+			pbfilebs := goutils.ReadFile(pb.AbsDir)
+			pbfilecnt := goutils.ToString(pbfilebs)
+
+			methodContains := strings.Contains(pbfilecnt, method)
+			if !methodContains && strings.Contains(pbfilecnt, "Methods: []grpc.MethodDesc") {
+				continue
+			}
+
+			log.Infof("%s --> %s", method, pb.AbsDir)
+
+			pbdesc = getDescriptorBytes(pbfilebs)
 			var err error
 			pbdescfile, err = decodeDescByRaw(pbdesc)
 			if err != nil {
@@ -71,14 +98,12 @@ func (p *ProtoDesc) searchDescSrc(method string) (grpcurl.DescriptorSource, erro
 				continue
 			} else {
 				find = true
-				break
+				p.addDesc(pb.ImportName(), pbdesc, pbdescfile)
 			}
 		}
 		if !find {
 			panic(fmt.Sprintf("not found pb for:%s", method))
 		}
-		p.rawPbDescs = append(p.rawPbDescs, pbdesc)
-		p.fileDescriptorSet.File = append(p.fileDescriptorSet.File, pbdescfile)
 	}
 	return source, nil
 }
